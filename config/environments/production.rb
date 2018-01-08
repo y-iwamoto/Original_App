43396b41147d7322d0bc4a2bfad57396d24a7570
YukiIwamoto <yukiiwamoto@YukiIwamotonoMacBook-Pro.local>
2018-01-08 12:37:16 +0900

    change producte mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "originalApp_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'ec2-13-230-180-23.ap-northeast-1.compute.amazonaws.com' }
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['GMAIL_ADRESS'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  #config.action_mailer.delivery_method = :smtp
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
